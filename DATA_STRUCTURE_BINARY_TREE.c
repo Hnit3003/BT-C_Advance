ae0477f522c8c497af81e2331de35e8de0fbfbc4
TinhChau <115681261+Hnit3003@users.noreply.github.com>
2022-12-06 18:50:55 +0700

    Binary Tree (Node_Create, Insert, Print)

---
+++ DATA_STRUCTURE_BINARY_TREE.c
@@ -0,0 +1,101 @@
+/*---------------------------DATA_STRUCTURE_BINARY_TREE-------------------------------------------------------
+This Tree has the Odd data on the LeftChild and the Even data on the RightChild
+
+- NODE_Create(int NODE_Data): Create new node for the Tree, this node has the *LeftChild = *RightChild = NULL 
+                              and data = NODE_Data
+- void BINARY_TREE_Print(NODE NODE_ROOT, int TREE_Level): Print the Tree into the Terminal
+                                                          if Tree_Level = 0, all Data are printed
+            The Tree has format:
+            NODE_ROTO
+                |_RightChild
+                  |_RightChild...
+                |_LeftChild
+                  |_LeftChild...
+--------------------------------------------------------------------------------------------------------------*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct NODE
+{
+    int data;
+    struct NODE *RightChild;
+    struct NODE *LeftChild;
+   
+};
+
+typedef struct NODE *NODE;
+
+NODE NODE_Create(int NODE_Data)
+{
+    NODE NODE_New;
+    NODE_New = (NODE)malloc(sizeof(NODE));
+    NODE_New->data = NODE_Data;
+    NODE_New->LeftChild = NULL;
+    NODE_New->RightChild = NULL;
+
+    return NODE_New;
+}
+
+void NODE_Insert(NODE *NODE_ROOT, int NODE_Data)
+{
+    NODE NODE_New = NODE_Create(NODE_Data);
+    NODE Temp = NULL;
+    if(*NODE_ROOT == NULL)
+    {
+        *NODE_ROOT = NODE_New;
+    }else
+    {
+        Temp = *NODE_ROOT;
+
+        if(NODE_Data%2 == 0)
+        {
+           while(Temp->LeftChild != NULL)
+           {
+                Temp = Temp->LeftChild;
+           }
+           Temp->LeftChild = NODE_New;
+        }else
+        {
+            while(Temp->RightChild != NULL)
+            {
+                Temp = Temp->RightChild;
+            }
+            Temp->RightChild = NODE_New;
+        }
+    }
+}
+
+void BINARY_TREE_Print(NODE NODE_ROOT, int TREE_Level)
+{
+    if(NODE_ROOT == NULL)
+        return;
+    
+    for(int i = 0; i < TREE_Level; i++)
+    {
+        printf(i == TREE_Level - 1 ? "|_" : "  ");
+    }
+    printf("%d\n", NODE_ROOT->data);
+    BINARY_TREE_Print(NODE_ROOT->LeftChild, TREE_Level + 1);
+    BINARY_TREE_Print(NODE_ROOT->RightChild, TREE_Level + 1);
+}
+
+int main(int argc, char const *argv[])
+{
+    NODE NODE_ROOT1 = NULL;
+    
+    //Initialize the data for the BINARY_TREE
+    NODE_Insert(&NODE_ROOT1, 1);
+    NODE_Insert(&NODE_ROOT1, 2);
+    NODE_Insert(&NODE_ROOT1, 3);
+    NODE_Insert(&NODE_ROOT1, 4);
+    NODE_Insert(&NODE_ROOT1, 5);
+    NODE_Insert(&NODE_ROOT1, 7);
+    NODE_Insert(&NODE_ROOT1, 9);
+    NODE_Insert(&NODE_ROOT1, 6);
+    NODE_Insert(&NODE_ROOT1, 8);
+    
+    //Print all the BINARY_TREE
+    BINARY_TREE_Print(NODE_ROOT1, 0);
+    return 0;
+}
