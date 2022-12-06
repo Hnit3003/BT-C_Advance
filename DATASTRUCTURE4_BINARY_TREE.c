412c9625539d87bca149396e222ee8ccfe2b4202
TinhChau <115681261+Hnit3003@users.noreply.github.com>
2022-12-06 23:23:09 +0700

    Binary Tree (Node_Create, Insert, Print)

--- DATASTRUCTURE4_BINARY_TREE.c
+++ DATASTRUCTURE4_BINARY_TREE.c
@@ -1,16 +1,19 @@
 /*---------------------------DATA_STRUCTURE_BINARY_TREE-------------------------------------------------------
-This Tree has the Odd data on the LeftChild and the Even data on the RightChild
+*This Tree has the Odd data on the LeftChild and the Even data on the RightChild
 
 - NODE_Create(int NODE_Data): Create new node for the Tree, this node has the *LeftChild = *RightChild = NULL 
                               and data = NODE_Data
-- void BINARY_TREE_Print(NODE NODE_ROOT, int TREE_Level): Print the Tree into the Terminal
+- BINARY_TREE_Print(NODE NODE_ROOT, int TREE_Level): Print the Tree into the Terminal
                                                           if Tree_Level = 0, all Data are printed
-            The Tree has format:
-            NODE_ROTO
-                |_RightChild
-                  |_RightChild...
-                |_LeftChild
-                  |_LeftChild...
+            *The Tree has the form:
+                NODE_ROOT
+                    |_RightChild
+                      |_RightChild...
+                        |_...
+                    |_LeftChild
+                      |_LeftChild...
+                        |_...
+
 --------------------------------------------------------------------------------------------------------------*/
 
 #include<stdio.h>
@@ -37,6 +40,7 @@ NODE NODE_Create(int NODE_Data)
     return NODE_New;
 }
 
+/*[1] : [Insert] - ADD a New Branch Node on the Tree----------------------------------------------------------*/
 void NODE_Insert(NODE *NODE_ROOT, int NODE_Data)
 {
     NODE NODE_New = NODE_Create(NODE_Data);
@@ -82,20 +86,46 @@ void BINARY_TREE_Print(NODE NODE_ROOT, int TREE_Level)
 
 int main(int argc, char const *argv[])
 {
-    NODE NODE_ROOT1 = NULL;
-    
-    //Initialize the data for the BINARY_TREE
-    NODE_Insert(&NODE_ROOT1, 1);
-    NODE_Insert(&NODE_ROOT1, 2);
-    NODE_Insert(&NODE_ROOT1, 3);
-    NODE_Insert(&NODE_ROOT1, 4);
-    NODE_Insert(&NODE_ROOT1, 5);
-    NODE_Insert(&NODE_ROOT1, 7);
-    NODE_Insert(&NODE_ROOT1, 9);
-    NODE_Insert(&NODE_ROOT1, 6);
-    NODE_Insert(&NODE_ROOT1, 8);
-    
-    //Print all the BINARY_TREE
-    BINARY_TREE_Print(NODE_ROOT1, 0);
+    NODE NODE_ROOT_1 = NULL;
+    int OPTION = 0;
+
+    do
+    {
+
+        printf("*----------------------------------------[OPTION]----------------------------------------*\n");
+        printf("[1] : [Insert] - ADD a New Branch Node on the Tree\n");
+        printf("[2] : [Exit] - Complete the Binary Tree and exit program\n");
+        printf("*-----------------------------------******************-----------------------------------*\n");
+
+        printf("PLEASE CHOOSE THE OPTION: ");
+        scanf("%d", &OPTION);
+        
+        while((OPTION < 0) || (OPTION > 2))
+        {
+            printf("ERROR!! Invalid option {OPTION = %d}.\nPlease choose again: ", OPTION);
+            scanf("%d", &OPTION);
+        }
+
+        printf("\n");
+        int NODE_Data = 0;
+
+        switch (OPTION)
+        {
+        case 1:
+            printf("ENTER NEW DATA TO OF THE NODE: ");
+            scanf("%d", &NODE_Data);
+            NODE_Insert(&NODE_ROOT_1, NODE_Data);
+            break;
+
+        default:
+            break;
+        }
+
+        BINARY_TREE_Print(NODE_ROOT_1, 0);   
+
+    } while (OPTION != 2);
+
+    printf("\nEXIT...");
+
     return 0;
 }
